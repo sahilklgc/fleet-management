bd535d8d0beaee645833818e3c73b81b3a145039
Sahil Kotian <sahil.kotian316@gmail.com>
2026-04-22 01:24:17 -0400

    fix: allow larger import payloads

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,19 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./modules/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       origin: true,
       credentials: true
     }
   });
 
+  app.useBodyParser("json", { limit: "10mb" });
+  app.useBodyParser("urlencoded", { extended: true, limit: "10mb" });
   app.setGlobalPrefix("api");
   app.useGlobalPipes(
     new ValidationPipe({
